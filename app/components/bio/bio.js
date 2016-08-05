da4069b8dad46389f184154610175895e4bb9aa2
angeloLuna <angel.luna.najera@gmail.com>
2016-08-04 21:30:36 -0500

    added factory for experience.json, added experience.json that contains all data about Bio-experience. modified bio.html, actually use a only one card with ng-repeat.

--- app/components/bio/bio.js
+++ app/components/bio/bio.js
@@ -6,11 +6,22 @@
         templateUrl: 'app/components/bio/bio.html'
     }
 
+
+        function bioCtrl(expData) {
+            var bio = this;
+
+            bio.exp = null;
+
+            expData.query()
+            .$promise
+                .then(function(response){
+                    bio.exp = response;
+                    console.log(bio.exp);
+                })
+
+        }
+
     angular
         .module('personal')
         .component('bioTag', bio)
-
-        function bioCtrl() {
-            bio = this;
-        }
 })();
